eea7f5a602918c96074c5b6aa9f5e7507f407d95
unknown <you@example.com>
2023-06-28 10:02:15 +0200

    add change logo and name

--- Sneaker/src/components/Header/Logo.jsx
+++ Sneaker/src/components/Header/Logo.jsx
@@ -5,8 +5,8 @@ export default function Logo() {
     return (
 
         <div class="logo">
-                <img src="https://img.freepik.com/premium-vector/sneakers-shop-logo-design-shoes-store-sneaker-vector-illustration_668007-99.jpg?w=740" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
-                Sneaker
+                <img src="https://img.freepik.com/free-vector/random-female-shoes-flat-illustrations-set-summer-autumn-winter-foot-wear-women-moccasins-boots-trainers-heels-isolated-white_74855-20699.jpg?w=740&t=st=1687939255~exp=1687939855~hmac=9dd0683d6ee80d70667b392aa1552d76859b4b2e337885e3255179af5bba33f6" />
+                Khumalo Sneaker
         </div>
     )
 }
