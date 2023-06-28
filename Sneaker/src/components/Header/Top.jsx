6c5cd9bdfc18e403f85446502c43380734539f71
unknown <you@example.com>
2023-06-28 08:57:28 +0200

    add text and search

--- Sneaker/src/components/Header/Top.jsx
+++ Sneaker/src/components/Header/Top.jsx
@@ -4,23 +4,28 @@ import './Top.css';
 function Top() {
     return (
         <>
-        <div>
-            <a href="#" class="d-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2">
-                CALL: +011 983 3222
-            </a>
-            <a href="#" class="d-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2">
-                MY WISHLIST (0)
-            </a>
-            <a href="#" class="d-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2">
-                ABOUT US
-            </a>
-            <a href="#" class="d-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2">
-                CONTACT US
-            </a>
-            <a href="#" class="d-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2">
-                Login
-            </a>
-        </div>
+            <div>
+                <a href="#" class="d-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2">
+                    Find a store
+                </a>
+                <a href="#" class="d-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2">
+                    Help
+                </a>
+                <a href="#" class="d-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2">
+                    Join Us
+                </a>
+
+                <li class="nav-item dropdown">
+                    <a class="nd-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Sign IN
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Login</a></li>
+                        <li><a class="dropdown-item" href="#">Register</a></li>
+
+                    </ul>
+                </li>
+            </div>
         </>
     )
 }
