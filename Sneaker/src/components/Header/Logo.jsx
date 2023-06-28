221dadf254480b22b6ef989de18158ebb56f5d80
unknown <you@example.com>
2023-06-28 11:16:58 +0200

    add logo and font size

--- Sneaker/src/components/Header/Logo.jsx
+++ Sneaker/src/components/Header/Logo.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./Logo.css";
 
+
 export default function Logo() {
     return (
 
         <div class="logo">
-                <img src="https://img.freepik.com/free-vector/random-female-shoes-flat-illustrations-set-summer-autumn-winter-foot-wear-women-moccasins-boots-trainers-heels-isolated-white_74855-20699.jpg?w=740&t=st=1687939255~exp=1687939855~hmac=9dd0683d6ee80d70667b392aa1552d76859b4b2e337885e3255179af5bba33f6" />
-                Khumalo Sneaker
+                <img src="https://img.freepik.com/free-vector/random-female-shoes-flat-illustrations-set-summer-autumn-winter-foot-wear-women-moccasins-boots-trainers-heels-isolated-white_74855-20699.jpg?w=740&t=st=1687939255~exp=1687939855~hmac=9dd0683d6ee80d70667b392aa1552d76859b4b2e337885e3255179af5bba33f6"/>
+                 <span>Khumalo Sneaker</span>
         </div>
     )
 }
