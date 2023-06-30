e5f3e2a23048ef5dbe14e3157897926158c68e80
unknown <you@example.com>
2023-06-30 12:46:22 +0200

    add register and login for css , linked

--- Sneaker/src/components/Page/Register.jsx
+++ Sneaker/src/components/Page/Register.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
+import "./Login.css"
 
 function Register() {
-  return (
-    <div><div><form>
-    <div className="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-    </div>
-    <div className="mb-3">
-      <label for="exampleInputPassword1" class="form-label">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1"/>
-    </div>
-    <div className="mb-3 form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form></div></div>
-  )
+    return (
+        <div>
+            <div className='login'></div>
+            <div className="box">
+                <div className="header">Register</div>
+                <div><input className="form-control" type="text" placeholder="Name" fdprocessedid="0bsxl5" />
+                </div><div><input className="form-control" type="text" placeholder="Email" fdprocessedid="alopl4u" />
+                </div><div><input className="form-control" type="password" placeholder="Password" fdprocessedid="3mt0w"/>
+                </div><a href="/">Do you have an Account?</a><button fdprocessedid="la775g">Register</button>
+            </div>
+        </div>
+    )
 }
 
 export default Register
