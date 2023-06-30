5921bec2725ebbeae618b4d264c8bc220fb48c00
unknown <you@example.com>
2023-06-30 12:52:48 +0200

    check

--- Sneaker/src/components/Page/Register.jsx
+++ Sneaker/src/components/Page/Register.jsx
@@ -7,8 +7,8 @@ function Register() {
             <div className='login'></div>
             <div className="box">
                 <div className="header">Register</div>
-                <div><input className="form-control" type="text" placeholder="Name" fdprocessedid="0bsxl5" />
-                </div><div><input className="form-control" type="text" placeholder="Email" fdprocessedid="alopl4u" />
+                <div><input className="form-control" type="text" placeholder="Name" fdprocessedid="0bsxl5"/>
+                </div><div><input className="form-control" type="text" placeholder="Email" fdprocessedid="alopl4u"/>
                 </div><div><input className="form-control" type="password" placeholder="Password" fdprocessedid="3mt0w"/>
                 </div><a href="/">Do you have an Account?</a><button fdprocessedid="la775g">Register</button>
             </div>
