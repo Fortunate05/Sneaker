af6640dc66a87cf5d26315a8cc9bdbca9a1f2aea
unknown <you@example.com>
2023-06-30 12:58:42 +0200

    check register display they work

--- Sneaker/src/components/Page/Register.jsx
+++ Sneaker/src/components/Page/Register.jsx
@@ -4,14 +4,15 @@ import "./Login.css"
 function Register() {
     return (
         <div>
-            <div className='login'></div>
+            <div className='login'>
             <div className="box">
                 <div className="header">Register</div>
                 <div><input className="form-control" type="text" placeholder="Name" fdprocessedid="0bsxl5"/>
                 </div><div><input className="form-control" type="text" placeholder="Email" fdprocessedid="alopl4u"/>
                 </div><div><input className="form-control" type="password" placeholder="Password" fdprocessedid="3mt0w"/>
-                </div><a href="/">Do you have an Account?</a><button fdprocessedid="la775g">Register</button>
+                </div><a href="/login">Do you have an Account?</a><button fdprocessedid="la775g">Register</button>
             </div>
+        </div>
         </div>
     )
 }
